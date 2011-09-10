5c0fcbaaf4c15d55d5b835d8d1b02d58906709ba
Tomasz Figa <tomasz.figa@gmail.com>
2011-09-10 17:28:54 +0200

    usb: gadget: android: Report maximum vbus draw as set in kernel config.

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -204,7 +204,7 @@ static struct usb_configuration android_config_driver = {
 	.setup		= android_setup_config,
 	.bConfigurationValue = 1,
 	.bmAttributes	= USB_CONFIG_ATT_ONE | USB_CONFIG_ATT_SELFPOWER,
-	.bMaxPower	= 0xFA, /* 500ma */
+	.bMaxPower	= (CONFIG_USB_GADGET_VBUS_DRAW) >> 1,
 };
 
 static int android_setup_config(struct usb_configuration *c,
