c13669069dc355d4d32f44c0afef3e5bf8fa660c
Tomasz Figa <tomasz.figa@gmail.com>
2011-09-26 17:44:33 +0200

    power: spica-battery: Update battery status value independently from charger control.

--- drivers/power/spica_battery.c
+++ drivers/power/spica_battery.c
@@ -370,8 +370,6 @@ static void spica_battery_work(struct work_struct *work)
 	if (bat->chg_enable == chg_enable)
 		goto no_change;
 
-	bat->status = POWER_SUPPLY_STATUS_DISCHARGING;
-
 	if (chg_enable) {
 #ifdef CONFIG_HAS_WAKELOCK
 		wake_lock(&bat->chg_wakelock);
@@ -379,12 +377,13 @@ static void spica_battery_work(struct work_struct *work)
 		/* Enable the charger */
 		gpio_set_value(pdata->gpio_en, !pdata->gpio_en_inverted);
 
-		bat->status = POWER_SUPPLY_STATUS_CHARGING;
 		bat->fault = 0;
 #ifdef CONFIG_HAS_WAKELOCK
 		wake_unlock(&bat->fault_wakelock);
 #endif
 	} else {
+		bat->status = POWER_SUPPLY_STATUS_DISCHARGING;
+
 		/* Disable the charger */
 		gpio_set_value(pdata->gpio_en, pdata->gpio_en_inverted);
 
@@ -398,9 +397,12 @@ static void spica_battery_work(struct work_struct *work)
 	bat->chg_enable = chg_enable;
 
 no_change:
-	if (chg_enable &&
-	    !(gpio_get_value(pdata->gpio_chg) ^ pdata->gpio_chg_inverted))
-		bat->status = POWER_SUPPLY_STATUS_FULL;
+	if (chg_enable) {
+		if (gpio_get_value(pdata->gpio_chg) ^ pdata->gpio_chg_inverted)
+			bat->status = POWER_SUPPLY_STATUS_CHARGING;
+		else
+			bat->status = POWER_SUPPLY_STATUS_FULL;
+	}
 
 	/* We're no longer accessing shared data */
 	mutex_unlock(&bat->mutex);
