b49303461919049881913d941053b2d19372ee77
Tomasz Figa <tomasz.figa@gmail.com>
2011-10-16 17:01:48 +0200

    Revert "ARM: s3c64xx: clock: Formatting fixes of sync-mode ARMCLK set rate."
    This reverts commit 512a4daab13681e3ec6502d70cb1b1bbe1cedd2b.

--- drivers/net/wireless/bcmdhd/dhd_linux_sched.c
+++ drivers/net/wireless/bcmdhd/dhd_linux_sched.c
@@ -26,7 +26,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/sched.h>
-#include <typedefs.h>
 #include <linuxver.h>
 
 int setScheduler(struct task_struct *p, int policy, struct sched_param *param)
