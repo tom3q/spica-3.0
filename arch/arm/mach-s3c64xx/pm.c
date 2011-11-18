364e22ac73ad485cc930fddfe8ef137aa1b89edd
Tomasz Figa <tomasz.figa@gmail.com>
2011-11-18 20:07:20 +0100

    ARM: s3c64xx: Save S3C64XX_SLEEP_CFG register in PM code.

--- arch/arm/mach-s3c64xx/pm.c
+++ arch/arm/mach-s3c64xx/pm.c
@@ -89,6 +89,7 @@ static struct sleep_save misc_save[] = {
 
 	SAVE_ITEM(S3C64XX_SDMA_SEL),
 	SAVE_ITEM(S3C64XX_MODEM_MIFPCON),
+	SAVE_ITEM(S3C64XX_SLEEP_CFG),
 };
 
 void s3c_pm_configure_extint(void)
